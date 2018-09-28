57f51a3c311abbe7b75b3eb5b007fa9c8bc98216
lemon-clown <me.clown.lemon@gmail.com>
2018-09-28 23:03:40 +0800

    feat: add util/fs-util.

---
+++ src/util/fs-util.ts
@@ -0,0 +1,118 @@
+import fs from 'fs-extra'
+import path from 'path'
+import { logger } from '@/util/logger'
+
+
+/**
+ * 判断路径 p 是否为文件
+ *
+ * @param p   文件的路径
+ * @return 路径 p 是否为文件
+ */
+export async function isFile(p: string | null): Promise<boolean> {
+  if (p == null) return false
+  if (!fs.existsSync(p)) return false
+  let stat = await fs.stat(p)
+  return stat.isFile()
+}
+
+
+/**
+ * 判断路径 p 是否为文件夹
+ *
+ * @param p   文件的路径
+ * @return 路径 p 是否为文件
+ */
+export async function isDirectory(p: string | null): Promise<boolean> {
+  if (p == null) return false
+  if (!fs.existsSync(p)) return false
+  let stat = await fs.stat(p)
+  return stat.isDirectory()
+}
+
+
+/**
+ * 检查路径是否存在，若不存在则结束程序
+ *
+ * @param p               待检查的路径
+ * @param checkDirectory  是否检查其是否为目录
+ * @param checkFile       是否检查其是否为文件
+ * @param message         提示信息
+ */
+export async function ensureExist(p: string | null, checkDirectory: boolean, checkFile: boolean, message?: string) {
+  if (p == null) {
+    logger.error(message == null? 'the path is null.': message)
+    process.exit(-1)
+  }
+  if (!fs.existsSync(p!)) {
+    logger.error(message == null? `${p} is not found.`: message)
+    process.exit(-1)
+  }
+  if (checkDirectory) {
+    if (await isDirectory(p)) return
+    logger.error(message == null? `${p} is not a directory.`: message)
+    process.exit(-1)
+  }
+  if (checkFile) {
+    if (await isFile(p)) return
+    logger.error(message == null? `${p} is not a file.`: message)
+    process.exit(-1)
+  }
+}
+
+
+/**
+ * 检查文件是否存在
+ *
+ * @param p
+ * @param message
+ * @see ensureExist(string, boolean, boolean, string)
+ */
+export const ensureFileExist = async (p: string | null, message?: string) => ensureExist(p, false, true, message)
+
+
+/**
+ * 检查文件夹是否存在
+ *
+ * @param p
+ * @param message
+ * @see ensureExist(string, boolean, boolean, string)
+ */
+export const ensureDirectoryExist = async (p: string | null, message?: string) => ensureExist(p, true, false, message)
+
+
+/**
+ * 找到最近包含目标文件的路径（不断往祖先目录回溯）
+ *
+ * @param p       搜索的路径
+ * @param target  目标文件名
+ */
+export async function findNearestTarget(p: string, target: string): Promise<string | null> {
+  if (!await isDirectory(p)) throw new Error(`\`${p}\` isn't a valid directory.`)
+  const absoluteTarget = path.resolve(p, target)
+  if (fs.existsSync(absoluteTarget)) return absoluteTarget
+  const parentDirectory = path.dirname(p)
+  if (parentDirectory != p) return await findNearestTarget(parentDirectory, target)
+  return null
+}
+
+
+/**
+ * 收集某个目录下的文件
+ *
+ * @param p           目录路径
+ * @param recursive   是否递归收集
+ * @return 该目录下的路径列表
+ */
+export async function collectFiles(p: string, recursive: boolean): Promise<string[]> {
+  let files = await fs.readdir(p)
+  let result: string[] = []
+  for (let file of files) {
+    file = path.resolve(p, file)
+    if (await isFile(file)) result.push(file)
+    else if (recursive && await isDirectory(file)) {
+      result.push(...await collectFiles(file, recursive))
+    }
+  }
+  return result
+}

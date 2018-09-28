0f5e1c3be7cec51b21bc0f7deaa22c473fd208d3
lemon-clown <me.clown.lemon@gmail.com>
2018-09-29 01:27:35 +0800

    feat: add demo.

---
+++ demo/template.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+
+typedef long long LL;
+typedef unsigned int uint;
+const LL  LNF = 0x3f3f3f3f3f3f3f3fLL;
+const int INF = 0x3f3f3f3f;
+const int MAXN = 200000 + 10;
+
+
+int main() {
+
+
+    return 0;
+}

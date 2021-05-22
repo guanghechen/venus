c7011079021cf30256fe0815ca4d9b1500fa666d
guanghechen <me.guanghechen@gmail.com>
2021-05-22 10:51:47 +0800

    :art:  improve: use inquirer instead of readline to genearte question prompt

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import loadCommand from '@/command'
-import { doneWithClose } from '@/util/cli'
 import { registerCommanderOptions } from '@barusu/chalk-logger'
 import program from 'commander'
 import manifest from '../package.json'
-
-// 改造 Commander 的 action 方法，使其在执行成功后退出程序（为了退出控制台输入）
-{
-  const fn = program.Command.prototype as any
-  const action = fn.action
-  fn.action = function (f: (...args: any[]) => Promise<void>) {
-    return action.call(this, doneWithClose(f))
-  }
-}
 
 program.version(manifest.version)
 
--- src/util/cli.ts
+++ src/util/cli.ts
@@ -1,82 +1,31 @@
-import chalk from 'chalk'
-import readline from 'readline'
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
+import inquirer from 'inquirer'
 
 /**
- * 在控制台发起一个询问，返回用户的输入（一行）
- *
- * @param desc          问题描述
- * @param clearConsole  是否清空控制台这几行（准备重新打印）
- * @return 用户的回答
- */
-export const question = async (
-  desc: string,
-  clearConsole?: boolean,
-): Promise<string> => {
-  return await new Promise<string>(resolve => {
-    rl.question(desc, (answer: string) => {
-      if (clearConsole) {
-        // 清空控制台若干行，准备重新打印
-        const lineCnt: number =
-          desc.split('\n').length + answer.split('\n').length
-        readline.cursorTo(process.stdout, 0)
-        readline.clearLine(process.stdout, 0)
-        for (let i = 1; i < lineCnt; ++i) {
-          readline.moveCursor(process.stdout, 0, -1)
-          readline.clearLine(process.stdout, 0)
-        }
-      }
-      resolve(answer)
-    })
-  })
-}
-
-/**
- * 发起一个是或不是的提问，返回用户的回答
- *
- * @param desc          问题描述
- * @param defaultValue  当输入为空白时的缺省值，默认为 false
- * @return 布尔值，true/false
- */
-export const yesOrNo = async (
-  desc: string,
-  defaultValue = false,
-): Promise<boolean> => {
-  // eslint-disable-next-line no-param-reassign
-  desc = chalk.white(`${desc}? (y/n) `)
-  const answer: string = await question(desc, true)
-  const flag: boolean = /^\s*(y|n|yes|no)\s*$/i.test(answer)
-    ? answer[0].toLowerCase() === 'y'
-    : defaultValue
-  console.log(desc + chalk.green(flag ? 'yes' : 'no'))
-  return flag
-}
-
-/**
- * 执行完之后结束程序
- * @param fn
- */
-export const doneWithClose =
-  (fn: (...args: any[]) => Promise<void>): any =>
-  (...args: any[]): void => {
-    ;(async () => {
-      await fn(...args)
-      rl.close()
-      process.exit(0)
-    })()
-  }
-
-/**
- * 收集可变参数的参数列表
+ * Collect variadic parameters
  *
  * @param arg
  * @param args
  */
-export const collectOptionArgs = (arg: string, args: string[]): string[] => {
+export function collectOptionArgs(arg: string, args: string[]): string[] {
   args.push(arg)
   return args
+}
+
+/**
+ * Prompt a question in command line, ask for a boolean answer.
+ * @param description
+ * @param defaultValue
+ * @returns
+ */
+export async function yesOrNo(
+  description: string,
+  defaultValue = false,
+): Promise<boolean> {
+  const answer = await inquirer.prompt({
+    type: 'checkbox',
+    name: 'yesOrNo',
+    message: description,
+    default: defaultValue,
+  })
+  return answer.yesOrNo
 }
